6896422a00e2f40135fb929660a4c68d8af02a3f
tainrty <2130942534@qq.com>
2024-11-18 12:35:29 +0800

    增删改查完成
    Signed-off-by: tainrty <2130942534@qq.com>

--- src/main/java/com/example/my_book_manager_system/entity/User.java
+++ src/main/java/com/example/my_book_manager_system/entity/User.java
@@ -1,16 +1,27 @@
 package com.example.my_book_manager_system.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 
+@TableName("user")  // 指定对应数据库表名
 public class User implements Serializable {
+
+    @TableId(value = "userid", type = IdType.AUTO)  // 标记主键字段，假设主键是自增的
     private Integer userid;
 
+    @TableField("username")  // 可选，字段名与数据库列名一致时可省略
     private String username;
 
+    @TableField("userpassword")  // 可选
     private String userpassword;
 
+    @TableField("isadmin")  // 可选
     private Byte isadmin;
 
+    // Getter 和 Setter 方法
     public Integer getUserid() {
         return userid;
     }
